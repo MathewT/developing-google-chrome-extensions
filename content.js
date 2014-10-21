bf00dcdf7605d0dc11b79807219a9859f4abaa5f
MathewT <mathew.thomas@longdivision.com>
2014-10-20 18:57:28 -0700

    Progress

--- content.js
+++ content.js
@@ -1,6 +1,17 @@
+
+
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 	switch(message.type) {
 		case "colors-div":
+      console.log("context select is happening.");
+			var whole_doc = document.getElementsByTagName("html")[0].innerHTML;
+      console.log("here comes whole_doc");
+      // alert(encodeURIComponent(whole_doc));
+      var bcRequest = new XMLHttpRequest();
+      alert(document.URL);
+      console.log(whole_doc);
+      
+
 			var divs = document.querySelectorAll("div");
 			if(divs.length === 0) {
 				alert("There are no any divs in the page.");
@@ -12,3 +23,4 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 		break;
 	}
 });
+
